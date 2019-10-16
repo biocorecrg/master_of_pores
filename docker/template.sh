109d1ef9fa2da3f601a75abb8d13cde262857d41
Luca Cozzuto <lucacozzuto@gmail.com>
2019-10-16 10:10:43 +0200

    Delete template.sh

--- docker/template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -cwd 
-#$ -q biocore-el7
-#$ -pe smp 1
-#$ -j y
-#$ -l virtual_free=80G
-#$ -l h_rt=12:00:00
-docker build -t biocorecrg/nanopore:0.8 .
-docker push biocorecrg/nanopore:0.8
